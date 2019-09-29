b81067d3deea69169b7590ed4b93c1ce8bd83449
MassimoEsperto <em.decarlo@gmail.com>
2019-09-29 21:53:23 +0200

    Create login-sign-in.component.ts

---
+++ src/app/login/login-sign-in/login-sign-in.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login-sign-in',
+  templateUrl: './login-sign-in.component.html',
+  styleUrls: ['../../../assets/css/styles_login.css']
+})
+export class LoginSignInComponent implements OnInit {
+
+  username:string;
+  password:string;
+  utente:User;
+
+  constructor(private router: Router,private service: LoginService) { }
+
+  ngOnInit() {
+  }
+
+
+  LoginUser(){
+    console.log("esername: "+this.username);
+    console.log("PASSWORD: "+this.password);
+   // this.utente = new User(this.username,this.password,'',1);
+    this.service.getLogin(this.username,this.password).subscribe((result: any) =>{this.utente=result, console.log(this.utente)});
+    
+    
+    
+    /*
+
+    localStorage.setItem('user', JSON.stringify(this.utente));
+    this.router.navigate(['/dashboard']);
+*/
+  }
+}
