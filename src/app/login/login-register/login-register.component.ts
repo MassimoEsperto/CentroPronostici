f7435d18e595b8d1e41cdedd4ae6f5165af6f1e7
MassimoEsperto <em.decarlo@gmail.com>
2019-09-29 21:53:32 +0200

    Create login-register.component.ts

---
+++ src/app/login/login-register/login-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-register',
+  templateUrl: './login-register.component.html',
+  styleUrls: ['../../../assets/css/styles_login.css']
+})
+export class LoginRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
