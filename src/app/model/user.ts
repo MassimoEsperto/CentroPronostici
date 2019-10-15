268047dc153f57ae366f75481f172317af5213fb
MassimoEsperto <em.decarlo@gmail.com>
2019-10-16 00:53:07 +0200

    Update user.ts

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,15 +1,5 @@
-export class User{
-
+export interface User{
     username:string;
-    password:string;
     email:string;
     ruolo:number;
-    
-    constructor(username:string,password:string,email:string,ruolo:number)
-    {
-        this.username=username;
-        this.password=password;
-        this.email=email;
-        this.ruolo=ruolo;
-    }
 }
