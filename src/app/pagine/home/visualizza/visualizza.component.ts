268620abd23b3856973dcf6cf2d073bd98d5bd94
MassimoEsperto <em.decarlo@gmail.com>
2020-02-02 22:01:05 +0100

    Create visualizza.component.ts

---
+++ src/app/pagine/home/visualizza/visualizza.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizza',
+  templateUrl: './visualizza.component.html',
+  styleUrls: ['./visualizza.component.css']
+})
+export class VisualizzaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
