1ce93eb06546789528a2054c34e4d6006cf60baa
MassimoEsperto <em.decarlo@gmail.com>
2020-02-02 21:57:38 +0100

    Create utente.ts

---
+++ src/app/classi/model/utente.ts
@@ -0,0 +1,13 @@
+export class Utente {
+    username:string;
+    email:string;
+    ruolo:number;
+    password:string;
+  
+      constructor(
+        username: string,
+        email: string,
+        ruolo:   number,
+        password: string) {}
+    }
+    
