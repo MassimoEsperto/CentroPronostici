b282d87de7136bcff071dd4000e7f15735b06d70
MassimoEsperto <em.decarlo@gmail.com>
2020-02-02 21:55:39 +0100

    Create schedina.ts

---
+++ src/app/classi/model/schedina.ts
@@ -0,0 +1,17 @@
+export class Schedina {
+    id_schedina:number;
+    id_partita:number;
+    partita:string;
+    tipo:number;
+    risultato:string;
+    username:string;
+   
+      constructor(
+        id_schedina:number,
+        id_partita:number,
+        partita:string,
+        tipo:number,
+        risultato:string,
+        username:string) {}
+    }
+    
