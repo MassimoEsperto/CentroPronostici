a016e234b274820cca6b03c332b9d06f3b08d876
MassimoEsperto <em.decarlo@gmail.com>
2020-02-02 21:55:12 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginHomeComponent } from './login/login-home/login-home.component';
-import { LoginRegisterComponent } from './login/login-register/login-register.component';
-import { LoginSignInComponent } from './login/login-sign-in/login-sign-in.component';
+import { LoginHomeComponent } from './pagine/login/login-home/login-home.component';
+import { LoginRegisterComponent } from './pagine/login/login-register/login-register.component';
+import { LoginSignInComponent } from './pagine/login/login-sign-in/login-sign-in.component';
 
 const routes: Routes = [
     { 
@@ -25,7 +25,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
