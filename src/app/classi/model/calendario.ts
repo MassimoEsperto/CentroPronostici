7d6144d3542445611d72a43520d77abd95886e87
MassimoEsperto <em.decarlo@gmail.com>
2020-02-02 21:55:29 +0100

    Create calendario.ts

---
+++ src/app/classi/model/calendario.ts
@@ -0,0 +1,15 @@
+export class Calendario {
+    id_calendario:number;
+    partita:string;
+    data:Date;
+    goalc:number;
+    goalt:number;
+   
+      constructor(
+        id_calendario:number,
+        partita:string,
+        data:Date,
+        goalc:number,
+        goalt:number) {}
+    }
+    
