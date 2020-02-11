eb09f13f648e879728182f4054e3bbb90bdac445
MassimoEsperto <em.decarlo@gmail.com>
2020-02-12 00:26:21 +0100

    Create pronostici.ts

---
+++ src/app/classi/model/pronostici.ts
@@ -0,0 +1,10 @@
+export class Pronostici {
+    id_utente:string;
+    id_schedina:string;
+    tempo:Date;   
+      constructor(
+        id_utente:string,
+        id_schedina:string,
+        tempo:Date) {}
+    }
+    
