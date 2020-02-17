4629229d3c2d734a85f904e89f06dbc45e96d681
MassimoEsperto <em.decarlo@gmail.com>
2020-02-18 00:28:58 +0100

    Create classifica.component.ts

---
+++ src/app/pagine/home/classifica/classifica.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classifica',
+  templateUrl: './classifica.component.html',
+  styleUrls: ['./classifica.component.css']
+})
+export class ClassificaComponent implements OnInit {
+
+  loading:boolean=false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
