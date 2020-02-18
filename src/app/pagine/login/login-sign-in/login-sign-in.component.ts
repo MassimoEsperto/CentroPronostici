51c894a5433bf0f8156c0392d2868f5e82baf995
MassimoEsperto <em.decarlo@gmail.com>
2020-02-18 23:55:08 +0100

    Update login-sign-in.component.ts

--- src/app/pagine/login/login-sign-in/login-sign-in.component.ts
+++ src/app/pagine/login/login-sign-in/login-sign-in.component.ts
@@ -1,5 +1,3 @@
-import { SUCCESS } from './../../../classi/utils/costanti';
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/classi/model/user';
@@ -8,49 +6,52 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login-sign-in',
   templateUrl: './login-sign-in.component.html',
-  styleUrls: ['../../../../assets/css/styles_login.css']
+  styleUrls: ['./login-sign-in.component.css']
 })
 export class LoginSignInComponent implements OnInit {
 
-  username:string;
-  password:string;
-  utente:User;
+  utente: User;
   error = '';
-  success = '';
-  isPresente:boolean;
+  isPresente: boolean;
 
-  constructor(private router: Router,private service: AuthService) { }
+  constructor(private router: Router, private service: AuthService) { }
 
   ngOnInit() {
-    this.isPresente=true;
+    this.isPresente = true;
+  }
+
+  /**
+   * 
+   * @param loggami effettivo loggin
+   */
+  login(loggami) {
+
+    let usr = loggami.value;
+
+    this.service.login(usr.username, usr.password)
+      .subscribe({
+
+        next: (result: any) => {
+          this.utente = result;
+
+          if (null != this.utente) {
+            this.service.setLogged(JSON.stringify(this.utente));
+            this.router.navigate(['/home/dashboard']);
+          }
+          else {
+            this.isPresente = false;
+          }
+        },
+        error: (error: any) => {
+
+          // Stampa messaggio d'errore
+          this.error = error
+
+        }
+      })
+
   }
 
 
-  LoginUser(){
-    console.log("esername: "+this.username);
-    console.log("PASSWORD: "+this.password);
-  
-   this.service.login(this.username,this.password).subscribe(
-     (result: any) =>{
-       this.utente=result;
-       console.log('risultato finale:'+result);
-       console.log('risultato finale:'+JSON.stringify(result));
-       console.log('risultato finale:'+this.utente);
-        if(null!=this.utente)
-        {
-          this.success = SUCCESS;         
-          this.service.setLogged(JSON.stringify(this.utente));
-          this.router.navigate(['/home/dashboard']);
-        }
-        else
-        {
-          this.isPresente=false;
-        }
-      },
-      (err) => {this.error = err,  console.log(this.error) }
-    );
- 
 
-  }
- 
 }
