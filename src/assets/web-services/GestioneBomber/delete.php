fb036db4f35855a00e0fb1f4b92e1443c1ae5be4
MassimoEsperto <em.decarlo@gmail.com>
2020-02-19 22:36:17 +0100

    Create delete.php

---
+++ src/assets/web-services/GestioneBomber/delete.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../connect_local.php';
+
+
+$nome = ($_GET['nome'] !== null && $_GET['nome'] !== '')? mysqli_real_escape_string($con, trim($_GET['nome'])) : false;
+
+
+if(!$nome)
+{
+  return http_response_code(400);
+}
+
+// Delete.
+$sql = "DELETE FROM `cannoniere` WHERE `nome` ='{$nome}' LIMIT 1";
+
+  if(mysqli_query($con,$sql))
+  {
+    
+    $bomber = ['nome' => $nome];
+    echo json_encode(['data'=>$bomber]);
+  }
+  else
+  {
+    http_response_code(422);
+  }
