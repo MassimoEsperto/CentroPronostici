3cda8a163ee8a238e3ace37cd9ca04e603a8ad8d
MassimoEsperto <em.decarlo@gmail.com>
2020-02-23 22:05:46 +0100

    Update gestione-pronostici.service.ts

--- src/app/services/gestione-pronostici.service.ts
+++ src/app/services/gestione-pronostici.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { Schedina } from '../classi/model/schedina';
 import { map, catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { throwError, Observable } from 'rxjs';
 import { HttpSenderService } from './http-sender-service';
 import { Pronostici } from '../classi/model/pronostici';
 
@@ -13,139 +13,158 @@ export class GestionePronosticiService extends HttpSenderService {
 
   scheda: Schedina[];
   schedaPiena: Schedina[];
-  pronostico:Pronostici[];
-  private risultatiClassici = ['1', 'X', '2', '1X', 'X2','OVER','UNDER','GOL','NOGOL','PARI','DISPARI'];
-  private risultatiEsatti = ['1-0', '2-0','3-0','4-0','5-0','2-1','3-1','4-1','5-1','3-2','4-2','5-2','4-3','5-3','5-4',
-                    '1-1','2-2','3-3','4-4','5-5',
-                    '0-1','0-2','0-3','0-4','0-5','1-2','1-3','1-4','1-5','2-3','2-4','2-5','3-4','3-5','4-5'];
-                    
-  
-  constructor(private http:HttpClient) {super();}
+  pronostico: Pronostici[];
 
-/**
- * 
- * @param username 
- * restituisce un id nuovo al quale associare la nuova scheda
- * ogni user avra una lista di id per ogni scommessa giocata
- */
-  newSchedina(username:string){
+  constructor(private http: HttpClient) { super(); }
+
+  /**
+   * 
+   * @param username 
+   * restituisce un id nuovo al quale associare la nuova scheda
+   * ogni user avra una lista di id per ogni scommessa giocata
+   */
+  newSchedina(username: string) {
     const params = new HttpParams()
       .set('username', username);
 
     return this.http.get(`${this.buildURL("GestioneSchedina/newSchedina")}`, { params: params })
-    .pipe(map((res) => {
-      console.log(res['data']);
-      
-      return res['data'].id_schedina;
-    }),
-    catchError(this.handleError));
-}
+      .pipe(map((res) => {
+        console.log(res['data']);
+
+        return res['data'].id_schedina;
+      }),
+        catchError(this.handleError));
+  }
 
 
 
-/**
- * 
- * @param id_schedina 
- * @param username 
- * restituisce la schedina vuota senza ancora i risultati
- */
-  schedinaVuota(id_schedina:number,username: string){
+  /**
+   * 
+   * @param id_schedina 
+   * @param username 
+   * restituisce la schedina vuota senza ancora i risultati
+   */
+  schedinaVuota(id_schedina: number, username: string) {
     const params = new HttpParams()
-    .set('id_schedina', id_schedina.toString()).set('username', username);
-   console.log('schedina vuota');
-    return this.http.get<Schedina[]>(`${this.buildURL("GestioneSchedina/schedinaVuota")}`, { params: params  })
-    .pipe(map((res) => {
-      console.log('ritorno schedina');
-      console.log("res: ",res['data']);
-      this.scheda = res['data'];
-      return this.scheda;
-  
+      .set('id_schedina', id_schedina.toString()).set('username', username);
+    console.log('schedina vuota');
+    return this.http.get<Schedina[]>(`${this.buildURL("GestioneSchedina/schedinaVuota")}`, { params: params })
+      .pipe(map((res) => {
+        console.log('ritorno schedina');
+        console.log("res: ", res['data']);
+        this.scheda = res['data'];
+        return this.scheda;
+
+      }),
+        catchError(this.handleError));
+  }
+
+  schedinaPiena(id_schedina: number) {
+    const params = new HttpParams()
+      .set('id_schedina', id_schedina.toString());
+    console.log('schedina piena');
+    return this.http.get<Schedina[]>(`${this.buildURL("GestioneSchedina/schedinaPiena")}`, { params: params })
+      .pipe(map((res) => {
+        console.log('ritorno schedina');
+        this.schedaPiena = res['data'];
+        return this.schedaPiena;
+
+      }),
+        catchError(this.handleError));
+  }
+
+  /**
+   * 
+   * @param pronostico 
+   * Inserimento dei pronostici
+   */
+  insert(pronostico: Schedina[]) {
+    console.log("pronostico", pronostico);
+    console.log(JSON.stringify(pronostico));
+
+    return this.http.post(`${this.buildURL("GestionePronostici/insert")}`, { data: pronostico })
+      .pipe(map((res) => {
+        console.log("ritorno", res['data']);
+        return res['data'];
+      }),
+        catchError(this.handleError));
+  }
+  /**
+   * 
+   * @param pronostico 
+   * modifica del pronostico
+   */
+  update(pronostico: Schedina) {
+    console.log("pronostico", pronostico);
+    console.log(JSON.stringify(pronostico));
+
+    return this.http.post(`${this.buildURL("GestionePronostici/update")}`, { data: pronostico })
+      .pipe(map((res) => {
+        const scheda = this.schedaPiena.find((item) => {
+          return item['id_partita'] === pronostico['id_partita'];
+        });
+        if (scheda) {
+          scheda['risultato'] = pronostico['risultato'];
+        }
+        return this.schedaPiena;
+
+        return res['data'];
+      }),
+        catchError(this.handleError));
+  }
+
+  delete(id_schedina:number): Observable<Pronostici[]> {
+    const params = new HttpParams()
+      .set('id_schedina', id_schedina.toString());
+
+    return this.http.get(`${this.buildURL("GestioneSchedina/deleteSchedina")}`, { params: params })
+      .pipe(map(res => {
+        console.log('delete effettuato');
+        console.log(res);
+        const filterid =this.pronostico = this.pronostico.filter(item => item.id_schedina !== id_schedina);    
+        return this.pronostico = filterid;
+      }),
+      catchError(this.handleError));
+  }
+
+  /**
+   * 
+   * @param username 
+   * @param pass 
+   * restituisce la lista di pronostici associati al tuo user
+   */
+  list(username: string) {
+    const params = new HttpParams()
+      .set('username', username);
+
+    return this.http.get<Pronostici[]>(`${this.buildURL("GestionePronostici/list")}`, { params: params })
+      .pipe(map((res) => {
+        console.log("res lista ", res['data']);
+        this.pronostico = res['data'];
+        return this.pronostico;
+
+      }),
+        catchError(this.handleError));
+  }
+
+  /**
+   * get e set della variabile che decide se si possano inserire ancora pronostici
+   */
+  getVariabile(): Observable<boolean> {
+    return this.http.get(`${this.buildURL("GestionePronostici/getVariabile")}`).pipe(
+      map((res) => {
+        return res['data'];
     }),
     catchError(this.handleError));
-}
+  }
+  setVariabile(variabile:number): Observable<boolean> {
+    const params = new HttpParams()
+      .set('variabile', variabile.toString());
 
-schedinaPiena(id_schedina:number){
-  const params = new HttpParams()
-  .set('id_schedina', id_schedina.toString());
- console.log('schedina piena');
-  return this.http.get<Schedina[]>(`${this.buildURL("GestioneSchedina/schedinaPiena")}`, { params: params  })
-  .pipe(map((res) => {
-    console.log('ritorno schedina');
-    this.schedaPiena = res['data'];
-    return this.schedaPiena;
-
-  }),
-  catchError(this.handleError));
-}
-
-/**
- * 
- * @param pronostico 
- * Inserimento dei pronostici
- */
-insert(pronostico: Schedina[]){
-  console.log("pronostico",pronostico);
-  console.log(JSON.stringify(pronostico));
-
- return this.http.post(`${this.buildURL("GestionePronostici/insert")}`, { data: pronostico })
-    .pipe(map((res) => {
-      console.log("ritorno",res['data']);
-      return res['data'];
-    }),
-    catchError(this.handleError));
-}
-/**
- * 
- * @param pronostico 
- * modifica del pronostico
- */
-update(pronostico: Schedina){
-  console.log("pronostico",pronostico);
-  console.log(JSON.stringify(pronostico));
-
- return this.http.post(`${this.buildURL("GestionePronostici/update")}`, { data: pronostico })
-    .pipe(map((res) => {
-      const scheda = this.schedaPiena.find((item) => {
-        return item['id_partita'] === pronostico['id_partita'];
-      });
-      if (scheda) {
-        scheda['risultato'] = pronostico['risultato'];
-      }
-      return this.schedaPiena;
-
-      return res['data'];
-    }),
-    catchError(this.handleError));
-}
-
-/**
- * 
- * @param username 
- * @param pass 
- * restituisce la lista di pronostici associati al tuo user
- */
-list(username:string){
-  const params = new HttpParams()
-  .set('username', username);
-
-  return this.http.get<Pronostici[]>(`${this.buildURL("GestionePronostici/list")}`, { params: params  })
-  .pipe(map((res) => {
-    console.log("res lista ",res['data']);
-    this.pronostico = res['data'];
-    return this.pronostico;
-
-  }),
-  catchError(this.handleError));
-}
-
-
-
-getRisultatiClassici() {
-  return this.risultatiClassici;
-}
-
-getRisultatiEsatti() {
-  return this.risultatiEsatti;
-}
-
+    return this.http.get(`${this.buildURL("GestionePronostici/setVariabile")}`, { params: params })
+      .pipe(map(res => {
+        return res['data'];
+      }),
+      catchError(this.handleError));
+  }
 }
