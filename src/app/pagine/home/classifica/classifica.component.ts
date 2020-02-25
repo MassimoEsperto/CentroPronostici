958d155f37d509aa356f10cd6e04f797f39ead99
MassimoEsperto <em.decarlo@gmail.com>
2020-02-25 22:41:55 +0100

    Update classifica.component.ts

--- src/app/pagine/home/classifica/classifica.component.ts
+++ src/app/pagine/home/classifica/classifica.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Pronostici } from 'src/app/classi/model/pronostici';
+import { Schedina } from 'src/app/classi/model/schedina';
+import { GestionePronosticiService } from 'src/app/services/gestione-pronostici.service';
+import { ClrDatagridSortOrder } from '@clr/angular';
 
 @Component({
   selector: 'app-classifica',
@@ -7,10 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassificaComponent implements OnInit {
 
-  loading:boolean=false;
-  constructor() { }
+  constructor(private service: GestionePronosticiService) { }
+
+  classifica:Pronostici[];
+  schede:Schedina[];
+
+  error = '';
+  success = '';
+  loading:boolean=true;
+
+ // descSort = ClrDatagridSortOrder.DESC;
 
   ngOnInit() {
+    this.getClassifica();
+  }
+
+
+  onViewScheda(scheda){
+    this.loading=true;
+    this.getSchedinaFinale(scheda.id_schedina);
+  }
+
+  getSchedinaFinale(id_schedina:number){
+
+    this.service.schedinaFinale(id_schedina)
+    .subscribe({
+   
+     next: (result: Schedina[]) => {
+      
+      
+       this.schede=result;
+       console.log("schedina piena",this.schede)
+     //  this.play=true;
+       this.loading=false;
+     },
+     error: (error: any) => {
+   
+       // Stampa messaggio d'errore
+       this.error = error
+   
+     }
+   })
+   
+   }
+
+  getClassifica(){
+  
+   this.service.classifica()
+   .subscribe({
+  
+    next: (result: Pronostici[]) => {
+  
+      this.classifica=result;
+      this.loading=false;
+  
+    },
+    error: (error: any) => {
+  
+      // Stampa messaggio d'errore
+      this.error = error
+  
+    }
+  })
+  
   }
 
 }
