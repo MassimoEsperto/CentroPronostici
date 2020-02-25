0a2b69027993f445f7c15dc0d9f45f973d23888b
MassimoEsperto <em.decarlo@gmail.com>
2020-02-26 00:34:22 +0100

    Update general-component.ts

--- src/app/classi/utils/general-component.ts
+++ src/app/classi/utils/general-component.ts
@@ -4,17 +4,43 @@ export class Generale {
 
     error = '';
     success = '';
+    loading: boolean = true;
 
     successo() {
         this.success = SUCCESS;
-        //  this.loading=false;
+
         setTimeout(() => {
-            this.success = '';
+            this.resetErrors();
         }, 5000);
     }
 
     resetErrors() {
         this.success = '';
         this.error = '';
+    }
+
+    stampaErrore(errore) {
+        this.error = errore
+    }
+
+    getRuoloStrng(rol) {
+        switch (rol) {
+            case "1": {
+                return "ADMIN"
+                break;
+            }
+            case "2": {
+                return "PLAYER";
+                break;
+            }
+            case "3": {
+                return "VISITATORE";
+                break;
+            }
+            default: {
+                return "VISITATORE";
+                break;
+            }
+        }
     }
 }
