026db55b59a5fc848a1cf09fa5900c7044a8f708
MassimoEsperto <em.decarlo@gmail.com>
2020-03-01 19:19:31 +0100

    Update update.php

--- src/assets/web-services/GestioneUtenti/update.php
+++ src/assets/web-services/GestioneUtenti/update.php
@@ -11,7 +11,7 @@ if(isset($postdata) && !empty($postdata))
 	
 
   // Validate.
-  if(trim($request->data->username) === '' || (int)$request->data->ruolo < 1)
+  if(trim($request->data->username) === '' || trim($request->data->ruolo)=== '' )
   {
     return http_response_code(400);
   }
@@ -19,10 +19,42 @@ if(isset($postdata) && !empty($postdata))
   // Sanitize.
   $username = mysqli_real_escape_string($con, trim($request->data->username));
   $email = mysqli_real_escape_string($con, trim($request->data->email)); 
-  $ruolo = mysqli_real_escape_string($con, (int)$request->data->ruolo);
+  $ruolo = mysqli_real_escape_string($con, trim($request->data->ruolo)); 
+  
+   $subject = "Modifica account";
+
+	$message = "
+	<html>
+	<head>
+	<title>HTML email</title>
+	</head>
+	<body>
+	<p>Account fantaeuropeo aggiornato!</p>
+	<table>
+	<tr>
+	<th>Username</th>
+	<th>Email</th>
+	<th>Ruolo</th>
+	</tr>
+	<tr>
+	<td>".$username."</td>
+	<td>".$email."</td>
+	<td>".$ruolo."</td>
+	</tr>
+	</table>
+	</body>
+	</html>
+	";
+
+	// Always set content-type when sending HTML email
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+	// More headers
+	$headers .= 'From: <pronostici@fantaeuropeo.com>' . "\r\n";
 
    // Update.
-  $sql = "UPDATE `utenti` SET `email`='{$email}',`ruolo`={$ruolo} WHERE `username` = '{$username}' LIMIT 1";
+  $sql = "UPDATE `utenti` SET `email`='{$email}',`ruolo`='{$ruolo}' WHERE `username` = '{$username}' LIMIT 1";
 
   if(mysqli_query($con,$sql))
   {
@@ -33,6 +65,7 @@ if(isset($postdata) && !empty($postdata))
       'ruolo' => $ruolo
     ];
     echo json_encode(['data'=>$car]);
+	mail($email,$subject,$message,$headers);
   }
   else
   {
