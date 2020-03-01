dfcc3222161dd8b0115c728fcb55a8776eda89e9
MassimoEsperto <em.decarlo@gmail.com>
2020-03-01 19:18:58 +0100

    Create enums.ts

---
+++ src/app/classi/utils/enums.ts
@@ -0,0 +1,22 @@
+
+export enum risultato {
+    ALTRO = 'ALTRO',
+    X = 'X',
+    UNOX = '1X',
+    XDUE = 'X2',
+    UNO = '1',
+    DUE = '2',
+    UNO_DUE = "12",
+    OVER = 'OVER',
+    UNDER = 'UNDER',
+    DISPARI = 'DISPARI',
+    PARI = 'PARI',
+    GOL = 'GOL',
+    NOGOL = 'NOGOL'
+}
+
+export enum Ruolo {
+    ADMIN = "admin",
+    GIOCATORE = "giocatore",
+    VISITATORE = "visitatore",
+}
