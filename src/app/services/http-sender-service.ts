368dcf4a58abf57ef0b57e53009610b61b94f6c2
MassimoEsperto <em.decarlo@gmail.com>
2020-03-03 21:12:59 +0100

    Update http-sender-service.ts

--- src/app/services/http-sender-service.ts
+++ src/app/services/http-sender-service.ts
@@ -5,52 +5,49 @@ import { Ruolo } from '../classi/utils/enums'
 
 export class HttpSenderService {
 
-  
-  
- buildURL(operation: string = ""): string {
- 
+
+
+  buildURL(operation: string = ""): string {
+
     let URL: string = WS_BASE_URL
-    
-    URL = URL + "/" + operation +".php"
+
+    URL = URL + "/" + operation + ".php"
 
     return URL
 
   }
 
-  username(){
+  username() {
     return JSON.parse(localStorage.getItem("tk-user")).username
   }
 
-  ruolo(){
+  ruolo() {
     return JSON.parse(localStorage.getItem("tk-user")).ruolo
   }
 
-  isadmin(){
-    let ruolo= this.ruolo();
-    if(ruolo==Ruolo.ADMIN){
+  isadmin() {
+    let ruolo = this.ruolo();
+    if (ruolo == Ruolo.ADMIN) {
       return true
     }
-    else
-    {
+    else {
       return false
     }
   }
-    
-  isPlayer(){
-    let ruolo= this.ruolo();
-    if(ruolo==Ruolo.ADMIN || ruolo==Ruolo.VISITATORE){
+
+  isPlayer() {
+    let ruolo = this.ruolo();
+    if (ruolo == Ruolo.ADMIN || ruolo == Ruolo.VISITATORE) {
       return true
     }
-    else
-    {
+    else {
       return false
     }
   }
-    
 
 
-   handleError(error: HttpErrorResponse) {
-    console.log(error);
+
+  handleError(error: HttpErrorResponse) {
 
     // return an observable with a user friendly message
     return throwError('Error! something went wrong.');
