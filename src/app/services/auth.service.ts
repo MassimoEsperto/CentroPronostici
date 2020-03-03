f8b7e9ae9ff7bd6edf8db490ddbf43d2e85f67b4
MassimoEsperto <em.decarlo@gmail.com>
2020-03-03 21:12:06 +0100

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,19 +2,19 @@ import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
-import { User } from 'src/app/classi/model/user';
+import { Token } from 'src/app/classi/model/token';
 import { HttpSenderService } from './http-sender-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService extends HttpSenderService {
-  users: User[];
+  token: Token;
   /**
    * Costruttore
    * @param http Servizio richieste HTTP
    */
-  constructor(private  http: HttpClient, private route: Router) {
+  constructor(private http: HttpClient, private route: Router) {
     super();
   }
 
@@ -23,25 +23,24 @@ export class AuthService extends HttpSenderService {
    * @param username Username
    * @param password Password
    */
-  login(username:string,pass: string){
+  login(username: string, pass: string) {
     const params = new HttpParams()
-    .set('user', username).set('pass', pass);
-   console.log('call servizio di login');
-    return this.http.get<User[]>(`${this.buildURL("GestioneUtenti/login")}`, { params: params  })
-    .pipe(map((res) => {
-      if('negato'==res['data'])
-      {
-        console.log('negato');
-        return null;
-      }
-    
-      this.users = res['data'];
-  
-      return this.users[0];
-  
-    }),
-    catchError(this.handleError));
-}
+      .set('user', username).set('pass', pass);
+
+    return this.http.get<Token[]>(`${this.buildURL("GestioneUtenti/login")}`, { params: params })
+      .pipe(map((res) => {
+        if ('negato' == res['data']) {
+
+          return null;
+        }
+
+        this.token = res['data'][0];
+
+        return this.token;
+
+      }),
+        catchError(this.handleError));
+  }
 
 
 
@@ -54,28 +53,39 @@ export class AuthService extends HttpSenderService {
     localStorage.removeItem("tk-user");
 
   }
-  
+
   /**
    * Verifica se l'utente è loggato
    */
   isLogged(): boolean {
 
     let token = localStorage.getItem("tk-user")
-    console.log("token",token);
-    let route = this.route.url
+    let now = new Date();
+    let scadenza: Date = new Date(JSON.parse(localStorage.getItem("tk-user")).scadenza);
 
-    // Ritorna true se il token è presente nella sessione
-
-    return !!token
+    // Ritorna true se il token è presente nella sessione  e nn sia scaduto
+    return !!token && now < scadenza 
 
   }
 
-  setLogged(tkuser :any){
+  setLogged(tkuser: any) {
 
     localStorage.setItem('tk-user', tkuser);
-    
+
   }
- 
+
+
+  recuperaPass(username: string, email: string) {
+    const params = new HttpParams()
+      .set('user', username).set('email', email);
+    return this.http.get<any>(`${this.buildURL("GestioneUtenti/recuperaPassword")}`, { params: params })
+      .pipe(map((res) => {
+
+        return 'ok';
+
+      }),
+        catchError(this.handleError));
+  }
 
 
 }
