e2585e9d9672ac9193b8a10e23ec422bc04b1b71
MassimoEsperto <em.decarlo@gmail.com>
2021-06-22 18:18:23 +0200

    Update pronostici.ts

--- src/app/classi/model/pronostici.ts
+++ src/app/classi/model/pronostici.ts
@@ -3,6 +3,7 @@ export class Pronostici {
     id_schedina:number;
     tempo:Date; 
     punteggio:number;  
+    indice:number; 
       constructor(
         id_utente:string,
         id_schedina:number,
