8596d3cf3698b28ae49d5d942a5eecf5ab1d613c
MassimoEsperto <em.decarlo@gmail.com>
2021-06-22 18:16:33 +0200

    Create tab-squadre.component.ts

---
+++ src/app/componenti/tab-squadre/tab-squadre.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Generale } from 'src/app/classi/utils/general-component';
+import { CreaCompetizioneService } from 'src/app/services/crea-competizione.service';
+
+@Component({
+  selector: 'tab-squadre',
+  templateUrl: './tab-squadre.component.html',
+  styleUrls: ['./tab-squadre.component.css']
+})
+export class TabSquadreComponent extends Generale implements OnInit {
+
+  @Input() squadre: any;
+  newTeam = '';
+  girone = '';
+
+  constructor(private creaService: CreaCompetizioneService) {
+    super();
+  }
+  ngOnInit() {
+  }
+
+    //CRUD 
+    onAddSquadra(nome: string,girone: string) {
+
+      this.resetErrors();
+      let gir=girone.toUpperCase()
+  
+      this.creaService.insertSquadra(nome,gir)
+        .subscribe({
+  
+          next: (result: any) => {
+  
+            this.squadre.push(result);
+            this.newTeam = '';
+  
+          },
+          error: (error: any) => {
+  
+            this.stampaErrore(error);
+  
+          }
+        })
+    }
+  
+    onDeleteSquadra(input: any) {
+  
+      this.resetErrors();
+      console.log("delete",input)
+  
+      this.creaService.deleteSquadra(input.nome)
+        .subscribe({
+  
+          next: (result: string) => {
+  
+            const index: number = this.squadre.findIndex(i => i.nome == result);
+            if (index !== -1) {
+              this.squadre.splice(index, 1);
+            }
+  
+  
+          },
+          error: (error: any) => {
+  
+            this.stampaErrore(error);
+  
+          }
+        })
+    }
+}
