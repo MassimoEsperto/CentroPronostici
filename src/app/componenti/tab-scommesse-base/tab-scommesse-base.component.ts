dfe1b56af9ac05726218fa4627894cd059227ce4
MassimoEsperto <em.decarlo@gmail.com>
2021-06-22 18:16:20 +0200

    Create tab-scommesse-base.component.ts

---
+++ src/app/componenti/tab-scommesse-base/tab-scommesse-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tab-scommesse-base',
+  templateUrl: './tab-scommesse-base.component.html',
+  styleUrls: ['./tab-scommesse-base.component.css']
+})
+export class TabScommesseBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
