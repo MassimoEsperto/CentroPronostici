e83cdd2a0f25acb6b6db02413dbc1e6a49222e6b
MassimoEsperto <em.decarlo@gmail.com>
2021-06-23 17:51:35 +0200

    Create getScommesseRisultati.php

---
+++ src/assets/web-services/GestioneCompetizione/getScommesseRisultati.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Returns the list of cars.
+ */
+require '../connect_local.php';
+    
+$list = [];
+$sql = "SELECT id_evento,tipo,partita,risesatto FROM _scommesse_risultati";
+
+if($result = mysqli_query($con,$sql))
+{
+  $cr = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $list[$cr]['id_evento'] = $row['id_evento'];
+    $list[$cr]['tipo'] = $row['tipo'];
+    $list[$cr]['descrizione'] = $row['partita'];
+    $list[$cr]['risultato'] = $row['risesatto'];
+    $cr++;
+  }
+    
+ echo json_encode(['data'=>$list]);
+}
+else
+{
+  http_response_code(404);
+}
