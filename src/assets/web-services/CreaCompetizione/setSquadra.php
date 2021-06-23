19d17b1eb762f0afa53b3ceeb7b1d7dcc18e486f
MassimoEsperto <em.decarlo@gmail.com>
2021-06-23 17:51:13 +0200

    Create setSquadra.php

---
+++ src/assets/web-services/CreaCompetizione/setSquadra.php
@@ -0,0 +1,25 @@
+<?php
+require '../connect_local.php';
+
+$nome = ($_GET['nome'] !== null && $_GET['nome'] !== '')? mysqli_real_escape_string($con, trim($_GET['nome'])) : false;
+$girone = ($_GET['girone'] !== null && $_GET['girone'] !== '')? mysqli_real_escape_string($con, trim($_GET['girone'])) : false;
+ 
+
+if(!$nome)
+{
+  return http_response_code(400);
+}
+
+
+  $sql = "INSERT INTO `_lista_squadre`(`nome`,`girone`) VALUES ('{$nome}','{$girone}')";
+
+  if(mysqli_query($con,$sql))
+  {
+    
+    $squadra = ['nome' => $nome];
+    echo json_encode(['data'=>$squadra]);
+  }
+  else
+  {
+    http_response_code(422);
+  }
