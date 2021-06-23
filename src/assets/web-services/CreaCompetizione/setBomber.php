cd509129b6eb2e9f796311f7a908f375567c1db2
MassimoEsperto <em.decarlo@gmail.com>
2021-06-23 17:51:05 +0200

    Create setBomber.php

---
+++ src/assets/web-services/CreaCompetizione/setBomber.php
@@ -0,0 +1,24 @@
+<?php
+require '../connect_local.php';
+
+$nome = ($_GET['nome'] !== null && $_GET['nome'] !== '')? mysqli_real_escape_string($con, trim($_GET['nome'])) : false;
+ 
+
+if(!$nome)
+{
+  return http_response_code(400);
+}
+
+
+  $sql = "INSERT INTO `_lista_cannonieri`(`nome`) VALUES ('{$nome}')";
+
+  if(mysqli_query($con,$sql))
+  {
+    
+    $bomber = ['nome' => $nome];
+    echo json_encode(['data'=>$bomber]);
+  }
+  else
+  {
+    http_response_code(422);
+  }
