7b83e420392d5eb9b812c0f26a4afe26b8b50197
MassimoEsperto <em.decarlo@gmail.com>
2021-06-23 17:52:28 +0200

    Update tab-scommesse-base.component.ts

--- src/app/componenti/tab-scommesse-base/tab-scommesse-base.component.ts
+++ src/app/componenti/tab-scommesse-base/tab-scommesse-base.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Generale } from 'src/app/classi/utils/general-component';
+import { GestioneCompetizioneService } from 'src/app/services/gestione-competizione.service';
 
 @Component({
   selector: 'tab-scommesse-base',
   templateUrl: './tab-scommesse-base.component.html',
   styleUrls: ['./tab-scommesse-base.component.css']
 })
-export class TabScommesseBaseComponent implements OnInit {
+export class TabScommesseBaseComponent extends Generale implements OnInit {
 
-  constructor() { }
+  @Input() combo: any;
+  scommesse: any = []
+  gruppo = '';
+  risultato = '';
 
+  constructor(private gestioneService: GestioneCompetizioneService) {
+    super();
+  }
   ngOnInit() {
+    this.getScommesseBase()
   }
 
+  onChangeSelect(e) {
+    this.risultato = ''
+  }
+
+  popolaCombo() {
+
+    switch (this.gruppo) {
+      case 'C':
+        return this.combo.cannonieri
+      case '':
+        return []
+
+      default:
+        return this.combo.team
+
+    }
+  }
+
+  //CRUD 
+  getScommesseBase() {
+
+    this.resetErrors();
+
+    this.gestioneService.getScommesseBase()
+      .subscribe({
+
+        next: (result: any) => {
+
+          this.scommesse = result
+
+
+        },
+        error: (error: any) => {
+
+          this.stampaErrore(error);
+
+        }
+      })
+  }
+
+  onDeleteRecord(input: any) {
+
+    this.resetErrors();
+
+    this.gestioneService.delScommesseBase(input.gruppo, input.risultato)
+      .subscribe({
+
+        next: (result: string) => {
+
+          this.getScommesseBase()
+
+        },
+        error: (error: any) => {
+
+          this.stampaErrore(error);
+
+        }
+      })
+  }
+
+  onAddRecord(gruppo, risultato) {
+
+    this.resetErrors();
+
+    this.gestioneService.setScommesseBase(gruppo, risultato)
+      .subscribe({
+
+        next: (result: string) => {
+
+          this.getScommesseBase()
+
+        },
+        error: (error: any) => {
+
+          this.stampaErrore(error);
+
+        }
+      })
+  }
 }
+
