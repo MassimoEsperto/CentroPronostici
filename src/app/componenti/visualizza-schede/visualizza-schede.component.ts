645a4d13096f0f3ccce269310ae7910e07181b66
MassimoEsperto <em.decarlo@gmail.com>
2021-06-28 19:59:45 +0200

    Create visualizza-schede.component.ts

---
+++ src/app/componenti/visualizza-schede/visualizza-schede.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Generale } from 'src/app/classi/utils/general-component';
+import { GestionePronosticiService } from 'src/app/services/gestione-pronostici.service';
+
+@Component({
+  selector: 'visualizza-schede',
+  templateUrl: './visualizza-schede.component.html',
+  styleUrls: ['./visualizza-schede.component.css']
+})
+export class VisualizzaSchedeComponent extends Generale implements OnInit {
+
+  @Input() pronostici: any = []
+
+  scheda_selected: any = []
+  viewScheda: boolean = false
+
+  constructor(private pronosticiService: GestionePronosticiService) {
+    super();
+  }
+
+  ngOnInit() { }
+
+  onViewScheda(element) {
+    this.getSchedaCompilata(element.id_schedina)
+  }
+
+
+  getSchedaCompilata(id_schedina: number) {
+
+    this.pronosticiService.getSchedaCompilata(id_schedina.toString())
+      .subscribe({
+
+        next: (result: any) => {
+
+          this.scheda_selected = result
+          this.viewScheda = true
+        },
+        error: (error: any) => {
+
+          this.stampaErrore(error);
+
+        }
+      })
+
+  }
+}
