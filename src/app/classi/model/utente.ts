735dc9334b6eae0e2778805af4e55f57d64a5aa8
MassimoEsperto <em.decarlo@gmail.com>
2021-07-09 18:24:41 +0200

    Update utente.ts

--- src/app/classi/model/utente.ts
+++ src/app/classi/model/utente.ts
@@ -1,18 +1,21 @@
 export class Utente {
-    username:string;
-    email:string;
-    ruolo:string;
-    password:string;
-    versato:number;
-    schede:number;
-    cellulare:string;
-  
-      constructor(
-        username: string,
-        email: string,
-        cellulare:string,
-        ruolo:   string,
-        password: string,
-        versato:number) {}
-    }
-    
+  username: string;
+  nome: string;
+  cognome: string;
+  email: string;
+  ruolo: string;
+  password: string;
+  versato: number;
+  schede: number;
+  cellulare: string;
+
+  constructor(
+    username: string,
+    nome: string,
+    cognome: string,
+    email: string,
+    cellulare: string,
+    ruolo: string,
+    password: string,
+    versato: number) { }
+}
