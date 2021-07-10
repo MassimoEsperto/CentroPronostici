81c7729e922ae1b5b1a21e0c73f8485ac0101676
MassimoEsperto <em.decarlo@gmail.com>
2021-07-11 00:55:26 +0200

    Update inserimento-scheda.component.ts

--- src/app/componenti/inserimento-scheda/inserimento-scheda.component.ts
+++ src/app/componenti/inserimento-scheda/inserimento-scheda.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { finalize } from 'rxjs/operators';
+import { defaultIfEmpty, finalize } from 'rxjs/operators';
 import { Generale } from 'src/app/classi/utils/general-component';
 import { GestionePronosticiService } from 'src/app/services/gestione-pronostici.service';
 
@@ -32,8 +32,7 @@ export class InserimentoSchedaComponent extends Generale implements OnInit {
   }
 
   onEditRisultati(ele) {
-    console.log("onEditelement", ele)
-    console.log("isAviable", this.isAviable())
+    console.log("this.scheda", this.scheda)
     this.selected = ele
   }
 
@@ -46,5 +45,32 @@ export class InserimentoSchedaComponent extends Generale implements OnInit {
     this.ritono.emit(this.scheda)
   }
 
+  onEditCompleto(ele, indice) {
+    let ris = this.scheda[indice - 4].risultato.substring(0, 3) + '-' + this.scheda[indice - 3].risultato.substring(0, 3) + '-' + this.scheda[indice - 2].risultato.substring(0, 3) + '-' + this.scheda[indice - 1].risultato.substring(0, 3)
+    return ris
+  }
+
+  onChange(element) {
+    let girone = this.scheda.filter(i => i.tipo == "3" && i.girone == element.girone);
+    let team = girone.filter(i => i.specie == "S");
+    let completo = girone.find(i => i.specie == "C");
+    let completoString = "";
+    let sep = "";
+    let diff = []
+
+    for (let item of team) {
+      let esiste = diff.some(i => i == item.risultato && i != "");
+      if (esiste) {
+        completoString = ""
+        break
+      } else {
+        completoString = completoString + sep + item.risultato.substring(0, 3)
+        sep = "-"
+        diff.push(item.risultato)
+      }
+    }
+
+    completo.risultato = completoString
+  }
 
 }
