c543e3f96a4fd43d1606d2ad873abd246b796751
MassimoEsperto <em.decarlo@gmail.com>
2021-07-17 01:29:05 +0200

    Update side-bar.component.ts

--- src/app/componenti/side-bar/side-bar.component.ts
+++ src/app/componenti/side-bar/side-bar.component.ts
@@ -4,6 +4,8 @@ import { Generale } from 'src/app/classi/utils/general-component';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { CommonService } from 'src/app/services/common.service';
+import { GestioneCompetizioneService } from 'src/app/services/gestione-competizione.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'side-bar',
@@ -14,10 +16,17 @@ export class SideBarComponent extends Generale implements OnInit {
   username: string;
   isAdmin: boolean;
   isPlayer: boolean;
-  variabile: boolean;
+  isOpen: boolean;
   opzioni: any;
+  combosel: any;
+  competizione:string;
 
-  constructor(private router: Router, private authservice: AuthService, private service: GestionePronosticiService, private commonService: CommonService) { super() }
+  constructor(
+    private router: Router, 
+    private authservice: AuthService, 
+    private service: GestionePronosticiService, 
+    private competizioneService: GestioneCompetizioneService, 
+    private commonService: CommonService) { super() }
 
   ngOnInit() {
     this.isAdmin = this.service.isadmin();
@@ -25,15 +34,16 @@ export class SideBarComponent extends Generale implements OnInit {
     this.isPlayer = this.service.isPlayer();
     this.loading = true;
     this.getOpzioni();
+    this.getCombo();
   }
 
 
   getOpzioni() {
 
-    this.commonService.getOpzioni()
+    this.commonService.getOpzioniAdmin()
       .subscribe({
         next: (result: any) => {
-          this.variabile = result.valore;
+          this.isOpen = result.isOpen;
           this.opzioni = result
         },
         error: (error: any) => {
@@ -42,7 +52,7 @@ export class SideBarComponent extends Generale implements OnInit {
   }
 
   upadateOpzioni(payload:any) {
-    this.commonService.updOpzioni(payload)
+    this.competizioneService.updOpzioni(payload)
     .subscribe({
       next: (result: any) => {
         this.successo()
@@ -55,7 +65,7 @@ export class SideBarComponent extends Generale implements OnInit {
 
   onUpdateOpzioni(opzioni) {
     let scelta = (opzioni.valore) ? 1 : 0;
-    let payload = { blocco: scelta, scadenza: opzioni.scadenza, testo: opzioni.testo }
+    let payload = { blocco: scelta, scadenza: opzioni.scadenza, footer: opzioni.footer }
     this.upadateOpzioni(payload)
   }
 
@@ -64,4 +74,26 @@ export class SideBarComponent extends Generale implements OnInit {
     this.router.navigate(['login']);
   }
 
+  
+  getCombo() {
+
+    this.commonService.getCombo()
+      .subscribe({
+
+        next: (result: any) => {
+          this.combosel = result;
+        },
+        error: (error: any) => {
+
+          this.stampaErrore(error);
+
+        }
+      })
+
+  }
+
+  onSelectCompetizione(element){
+    this.commonService.setCompetizione(element);
+    this.refreshPage();
+  }
 }
