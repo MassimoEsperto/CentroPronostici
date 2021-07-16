c55bdbdab7373db33e04d8ed18452d66b2d82840
MassimoEsperto <em.decarlo@gmail.com>
2021-07-17 01:28:13 +0200

    Update updOpzioni.php

--- src/assets/web-services/GestioneCompetizione/updOpzioni.php
+++ src/assets/web-services/GestioneCompetizione/updOpzioni.php
@@ -9,11 +9,11 @@ if(isset($postdata) && !empty($postdata))
   // Extract the data.
   $request = json_decode($postdata);
 
-  $testo = mysqli_real_escape_string($con, trim($request->data->testo)); 
+  $footer = mysqli_real_escape_string($con, trim($request->data->footer)); 
   $scadenza = mysqli_real_escape_string($con, trim($request->data->scadenza)); 
   $blocco = mysqli_real_escape_string($con, (int)$request->data->blocco);
 		  
-  $sql = "UPDATE `_opzioni` SET `open`={$blocco},`scadenza`='{$scadenza}',`testo`='{$testo}'";
+  $sql = "UPDATE `_opzioni` SET `isOpen`={$blocco},`scadenza`='{$scadenza}',`footer`='{$footer}' WHERE `comp_id` = {$id_comp}";
 
   if(mysqli_query($con,$sql))
   {
@@ -26,6 +26,7 @@ if(isset($postdata) && !empty($postdata))
   }
   else
   {
+  echo json_encode(['data'=>$sql]);
     http_response_code(422);
   }
 }
