fe6d14c3d947effa3aab49bdd52d163b34e9beb1
MassimoEsperto <em.decarlo@gmail.com>
2021-07-17 01:28:38 +0200

    Update getSchedaDaCompilare.php

--- src/assets/web-services/PronosticiUtente/getSchedaDaCompilare.php
+++ src/assets/web-services/PronosticiUtente/getSchedaDaCompilare.php
@@ -4,15 +4,15 @@ require '../connect_local.php';
 
 $ele = 0;
 $list = [];
-$sql1 ="SELECT r.id_evento,r.tipo,r.partita FROM _scommesse_risultati r order by r.id_evento";
+$sql1 ="SELECT r.id_evento,r.tipo,r.partita FROM _scommesse_risultati r WHERE r.comp_id = {$id_comp} order by r.id_evento";
 
 $sql2 = "SELECT b.id_evento,b.tipo,p.descrizione,b.gruppo_id ";
 $sql2 .="FROM _scommesse_antepost_base b,_punti_previsti p ";
 $sql2 .="WHERE b.punti_id=p.id_punti order by b.id_evento ";
  
-$sql3 = "SELECT s.id_evento,s.tipo,p.descrizione,s.girone ";
+$sql3 = "SELECT s.id_evento,s.tipo,p.descrizione,s.girone,s.specie ";
 $sql3 .="FROM _scommesse_antepost_gironi s,_punti_previsti p  ";
-$sql3 .="WHERE s.punti_id=p.id_punti order by s.id_evento ";
+$sql3 .="WHERE s.punti_id=p.id_punti AND s.comp_id = {$id_comp} order by s.id_evento ";
 
 
 if($result = mysqli_query($con,$sql1))
@@ -63,6 +63,7 @@ if($result = mysqli_query($con,$sql3))
     $list[$ele]['girone'] = $row['girone'];
     $list[$ele]['risultato'] = "";
     $list[$ele]['tipo'] = $row['tipo'];
+    $list[$ele]['specie'] = $row['specie'];
     $ele++;
   }
     
