401d4ec4f740c267b4ee420a1ce9cc47f57a8d8b
MassimoEsperto <em.decarlo@gmail.com>
2021-07-17 01:28:26 +0200

    Update getClassificaGenerale.php

--- src/assets/web-services/PronosticiUtente/getClassificaGenerale.php
+++ src/assets/web-services/PronosticiUtente/getClassificaGenerale.php
@@ -37,10 +37,20 @@ $sql .="END as 'punteggio' ";
 $sql .="FROM _schedina_user u,_schedina s,_scommesse_antepost_gironi g,_punti_previsti p "; 
 $sql .="WHERE g.punti_id=p.id_punti and u.schedina_id=s.id_schedina and g.id_evento= s.id_evento and g.tipo= s.tipo order by id_evento "; 
 $sql .="	) punti where schedina_id= fa.id_schedina "; 
-$sql .=") as pt ";
+$sql .=") as pt, ";
+$sql .="(SELECT SUM(avulsa) AS avulsa from ( ";
+$sql .="	SELECT b.id_evento,b.tipo,u.schedina_id, ";
+$sql .="CASE ";
+$sql .="WHEN (SELECT count(*) FROM _gruppi_antepost_base gb WHERE s.risultato=gb.risultato AND gb.gruppo=b.gruppo_id AND b.gruppo_id IN ('C','V')  )>0 THEN p.valore "; 
+$sql .="ELSE 0 "; 
+$sql .="END as 'avulsa' ";
+$sql .="FROM _schedina_user u,_schedina s,_scommesse_antepost_base b,_punti_previsti p ";
+$sql .="WHERE b.punti_id=p.id_punti and u.schedina_id=s.id_schedina and b.id_evento= s.id_evento and b.tipo= s.tipo "; 
+$sql .="	) punti where schedina_id= fa.id_schedina "; 
+$sql .=") as av ";
 $sql .="  FROM _schedina fa,_schedina_user fu "; 
-$sql .="  WHERE fa.id_schedina = fu.schedina_id ";
-$sql .="  group by fu.utente_id, fa.id_schedina order by pt DESC ";
+$sql .="  WHERE fa.id_schedina = fu.schedina_id AND fu.comp_id = {$id_comp}";
+$sql .="  group by fu.utente_id, fa.id_schedina order by pt DESC,av ASC ";
   
 
  
@@ -53,7 +63,9 @@ if($result = mysqli_query($con,$sql))
     $schede[$index]['id_schedina'] = $row['id_schedina'];
     $schede[$index]['tempo'] = $row['tempo'];
 	$schede[$index]['punteggio'] = $row['pt'];
+    $schede[$index]['punteggioString'] = substr("0000" . $row['pt'],strlen($row['pt']));
     $schede[$index]['posizione'] = $index+1;
+    $schede[$index]['posizioneString'] = substr("0000" . strval($index),strlen(strval($index)));
     $index++;
   }
     
