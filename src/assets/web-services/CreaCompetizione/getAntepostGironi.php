73fe68b33c7e7a32800271cf2269a5b7238cfeae
MassimoEsperto <em.decarlo@gmail.com>
2021-07-17 01:27:23 +0200

    Update getAntepostGironi.php

--- src/assets/web-services/CreaCompetizione/getAntepostGironi.php
+++ src/assets/web-services/CreaCompetizione/getAntepostGironi.php
@@ -3,7 +3,7 @@
 require '../connect_local.php';
     
 $element = [];
-$sql = "SELECT distinct girone FROM `_scommesse_antepost_gironi` order by id_evento";
+$sql = "SELECT distinct girone FROM `_scommesse_antepost_gironi` WHERE `comp_id` = {$id_comp} order by id_evento";
 
 if($result = mysqli_query($con,$sql))
 {
