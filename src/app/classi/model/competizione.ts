0adaa459475924401b4a3e1ebd1a8e609dd7b4fa
MassimoEsperto <em.decarlo@gmail.com>
2021-07-19 14:57:56 +0200

    Update competizione.ts

--- src/app/classi/model/competizione.ts
+++ src/app/classi/model/competizione.ts
@@ -3,16 +3,25 @@ export class Competizione {
     sigla: string;
     descrizione: string;
     isAttiva: string;
+    footer: string;
+    scadenza: string;
+    isOpen: string;
 
     constructor(
         id_comp: string,
         sigla: string,
         descrizione: string,
-        isAttiva?: string
+        isAttiva?: string,
+        footer?: string,
+        scadenza?: string,
+        isOpen?: string,
     ) { 
         this.id_comp=id_comp
         this.sigla=sigla
         this.descrizione=descrizione
         this.isAttiva=isAttiva
+        this.footer=footer
+        this.scadenza=scadenza
+        this.isOpen=isOpen
     }
 }
