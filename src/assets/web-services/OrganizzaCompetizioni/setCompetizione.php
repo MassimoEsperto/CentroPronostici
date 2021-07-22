aec008ec01f4ff52e3e989ba92b2a908fadf1368
MassimoEsperto <em.decarlo@gmail.com>
2021-07-22 15:51:53 +0200

    Update setCompetizione.php

--- src/assets/web-services/OrganizzaCompetizioni/setCompetizione.php
+++ src/assets/web-services/OrganizzaCompetizioni/setCompetizione.php
@@ -13,19 +13,8 @@ if(isset($postdata) && !empty($postdata))
   $descrizione = mysqli_real_escape_string($con, trim($request->data->descrizione));
 
   $sql = "INSERT INTO `_competizioni`(`sigla`,`descrizione`) VALUES ('{$sigla}','{$descrizione}')";
-  $sql2 = "";
   
   if(mysqli_query($con,$sql))
-  {
-  	$last_id = mysqli_insert_id($con);
-    $sql2 .="INSERT INTO `_opzioni`(`comp_id`) VALUES ({$last_id})";
-  }
-  else
-  {
-    http_response_code(420);
-  }
-  
-   if(mysqli_query($con,$sql2))
   {
     http_response_code(201);
     $obj = [
@@ -35,6 +24,8 @@ if(isset($postdata) && !empty($postdata))
   }
   else
   {
-    http_response_code(422);
+    http_response_code(420);
   }
+  
+  
 }
