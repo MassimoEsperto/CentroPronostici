ea4ed5a10567ded3b1b6ac0cade8bd0e03a35719
MassimoEsperto <em.decarlo@gmail.com>
2021-07-22 15:52:06 +0200

    Update getCompetizioni.php

--- src/assets/web-services/OrganizzaCompetizioni/getCompetizioni.php
+++ src/assets/web-services/OrganizzaCompetizioni/getCompetizioni.php
@@ -3,7 +3,8 @@
 require '../connect_local.php';
 
 $element = [];
-$sql = "SELECT `id_comp`,`sigla`,`descrizione`,`isAttiva` FROM `_competizioni`";
+
+$sql = "SELECT id_comp,sigla,descrizione,isAttiva,scadenza,footer,isOpen FROM _competizioni ";
 
 if($result = mysqli_query($con,$sql))
 {
@@ -14,6 +15,9 @@ if($result = mysqli_query($con,$sql))
     $element[$cr]['sigla'] = $row['sigla'];
 	$element[$cr]['descrizione'] = $row['descrizione'];	
 	$element[$cr]['isAttiva'] = $row['isAttiva'];	
+    $element[$cr]['scadenza'] = $row['scadenza'];
+	$element[$cr]['footer'] = $row['footer'];	
+	$element[$cr]['isOpen'] = $row['isOpen'];	
     $cr++;
   }
   
